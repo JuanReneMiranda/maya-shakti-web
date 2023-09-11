e57dbc05ef2a6e3fdebc2dff97add99452cc0d73
JuanReneMiranda <jrene.miranda@outlook.com>
2023-09-11 10:06:38 -0600

    Avance 3

--- dist/index.a7bae173.js
+++ dist/index.a7bae173.js
@@ -578,6 +578,18 @@ function hideListen() {
     $(".hide_listen").hide("slow", function() {});
 }
 setInterval(hideListen, 5500);
+<<<<<<< HEAD
+=======
+$(".ir-arriba").on("click", function() {
+    $("body, html").animate({
+        scrollTop: "0px"
+    }, 300);
+});
+$(window).on("scroll", function() {
+    if ($(this).scrollTop() > 0) $(".ir-arriba").slideDown(300);
+    else $(".ir-arriba").slideUp(300);
+});
+>>>>>>> be57b34 (Avance 2)
 
 },{}]},["3JqYA","av9Hc"], "av9Hc", "parcelRequire775a")
 
