4157c216641cb71e4d30c6a0f51317fb4bb557d0
JuanReneMiranda <jrene.miranda@outlook.com>
2023-09-14 11:20:23 -0600

    BUILD 1.2

--- dist/index.268d78f1.js
+++
@@ -1,3 +0,0 @@
-setInterval(function(){$(".hide_listen").hide("slow",function(){})},5500),$(".ir-arriba").on("click",function(){$("body, html").animate({scrollTop:"0px"},300)}),$(window).on("scroll",function(){$(this).scrollTop()>0?$(".ir-arriba").slideDown(300):$(".ir-arriba").slideUp(300)});//# sourceMappingURL=index.268d78f1.js.map
-
-//# sourceMappingURL=index.268d78f1.js.map
